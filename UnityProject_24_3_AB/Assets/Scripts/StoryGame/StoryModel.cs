5397b7b29aa61b1b980fac3906684495a8e39bdd
gamja54 <gghhjj867@gmail.com>
2024-05-24 11:28:10 +0900

    feat : "0524"

---
+++ UnityProject_24_3_AB/Assets/Scripts/StoryGame/StoryModel.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewStory" , menuName = "ScriptableObjects/StoryModel")]
+public class StoryModel : ScriptableObject
+{
+
+    public int storyNumber;
+    public Texture2D mainImage;
+
+    public enum STORYTYPE
+    {
+        MAIN,
+        SUB,
+        SERIAL
+    }
+
+    public STORYTYPE storyType;
+    public bool storyDone;
+
+    [TextArea(10, 10)]
+    public string storyText;
+
+    public Option[] options;
+
+    [System.Serializable]
+    public class Option
+    {
+        public string optionText;
+        public string buttonText;
+
+        public EventCheck eventCheck;
+    }
+
+    [System.Serializable]
+    public class EventCheck
+    {
+        public class EnentCheck
+        {
+            public int checkvalue;
+            public enum EventType : int
+            {
+                NONE,
+                GoToBattle,
+                CheckSTR,
+                CheckDEX,
+                CheckCON,
+                CheckINT,
+                CheckWIS,
+                CheckCHA
+            }
+
+            public EventType eventType;
+
+            public Result[] sucessResult;
+            public Result[] failResult;
+        }
+    }
+
+    [System.Serializable]
+    public class Result
+    {
+        public enum ResultType: int
+        {
+            changeHp,
+            ChangeSp,
+            AddExperience,
+            GoToShop,
+            GoToNextStory,
+            GoToRandomStory
+        }
+
+        public ResultType resultType;
+        public int value;
+        public Stats stats;
+    }
+}
